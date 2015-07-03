67ffa5fae5db875e2f553841bd230b4063472a07
Javier Carrillo <stringparser@gmail.com>
2015-07-03 19:37:29 +0200

    algorithm: plus-minus warmup

---
+++ algorithm/warmup/plus-minus.js
@@ -0,0 +1,34 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var count = [0, 0, 0];
+var length;
+
+process.stdin.on('data', function(data){
+  if(!data){ return; }
+
+  var input = data.match(/[+-]?\d+/g);
+  var integer;
+
+  var index = Number(input[0]) + 1;
+  length = index-1;
+
+  while(--index){
+    integer = Number(input[index]);
+    if(integer > 0){
+      ++count[0];
+    } else if(integer < 0){
+      ++count[1];
+    } else {
+      ++count[2];
+    }
+  }
+});
+
+process.stdin.on('end', function(){
+  count.forEach(function(num){
+    console.log(num/length);
+  });
+});
